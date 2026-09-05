818f07de376d6d7fe85d9a077f497217dd56f31f
Ritesh Kushwaha <riteshkushwahariteshkushwaha0@gmail.com>
2026-09-05 23:25:50 +0530

    Add Division class with divide method

---
+++ Division.java
@@ -0,0 +1,15 @@
+public class Division
+{
+ public static void main(String args[])
+{
+ int a= 100;
+ int b=25;
+ int c=divide(a,b);
+ System.out.println("Division of two number:"+c);
+}
+public static int divide(int n1,int n2)
+{
+ int divide=n1/n2;
+return divide;
+}
+}
