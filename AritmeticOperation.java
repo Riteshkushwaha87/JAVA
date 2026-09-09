4af957d0a6cf21d8f0a6e107d03a843ce2d99368
Ritesh Kushwaha <riteshkushwahariteshkushwaha0@gmail.com>
2026-09-09 17:42:40 +0530

    Create ArithmeticOperation class with basic methods
    Implement basic arithmetic operations in the ArithmeticOperation class.

---
+++ AritmeticOperation.java
@@ -0,0 +1,15 @@
+package Basics;
+
+import java.sql.SQLOutput;
+
+public class ArithmeticOperatin {
+    public static void main(String[] args){
+       // int x=10, y=4;      it will not return decimal value
+        // float x=8,y=3;      we can also store in float but in mostly prefer double
+        double x=9,y=2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
