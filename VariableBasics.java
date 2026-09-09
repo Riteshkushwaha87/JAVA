cc1500be5461a9a8a87fbb8cd42b78c72428a314
Ritesh Kushwaha <riteshkushwahariteshkushwaha0@gmail.com>
2026-09-09 11:36:58 +0530

    Add files via upload

---
+++ VariableBasics.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class VariableBasics {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x + 4);
+        int y = 5;
+        System.out.println(y);
+        System.out.println("HelloRitesh");
+    }
+}
