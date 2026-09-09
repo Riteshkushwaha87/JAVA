93e4a0e1206c7902b1a2c1e146fbbbd39dcbd3d6
Ritesh Kushwaha <riteshkushwahariteshkushwaha0@gmail.com>
2026-09-09 17:22:54 +0530

    Add files via upload

---
+++ AssignValue.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class AssignValue {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+        int y = 10;
+        System.out.println(y);
+        x = y + 3;
+        System.out.println(x);
+        x=y;//here we are assinging the value of y to x.
+        System.out.println(x);
+    }
+}
